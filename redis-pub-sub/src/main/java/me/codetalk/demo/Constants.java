097ea231ebfad252d55dcd8893a465a628d94c64
guobxu <gbxu@outlook.com>
2017-11-24 12:12:22 +0800

    complete demo

--- redis-pub-sub/src/main/java/me/codetalk/demo/Constants.java
+++ redis-pub-sub/src/main/java/me/codetalk/demo/Constants.java
@@ -5,15 +5,15 @@ public final class Constants {
 	public static final String ENCODING_UTF8 = "UTF-8";
 	
 	// channel: user
-	public static final String CHN_USER_CREATE = "user-create";
+	public static final String CHN_USER_CREATE = "demoapp-user-create";
 	
 	// message types
-	public static final String MSGTYPE_USER_CREATE = "user-create"; 
+	public static final String MSGTYPE_USER_CREATE = "demoapp-user-create"; 
 	
 	// channel: order
-	public static final String CHN_ORDER_CREATE = "order-create";
+	public static final String CHN_ORDER_CREATE = "demoapp-order-create";
 	
 	// message types
-	public static final String MSGTYPE_ORDER_CREATE = "order-create";
+	public static final String MSGTYPE_ORDER_CREATE = "demoapp-order-create";
 	
 }
